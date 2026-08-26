5ca89a6b47ceffe51d87117aa77052dc098f1e54
Ziya Nazreen <ziyanazreen879@gmail.com>
2026-08-26 22:26:46 +0530

    added new code

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let input = document.querySelector("input");
 let btn = document.querySelector("button");
 let foodResult = document.querySelector("#foodResult");
+foodResult.style.display = "none";
 btn.addEventListener("click",()=>{
     if(input.value != ""){
          console.log(input.value);
@@ -12,17 +13,27 @@ btn.addEventListener("click",()=>{
 async function getValue() {
     try{
         let result = await axios.get(`https://world.openfoodfacts.org/api/v0/product/${input.value}.json`);
-        // let product = result.data.product;
-        // foodResult.innerHTML = `<h2>${product.product_name}</h2>
-        //                 <h3><span>Brand:<span>${product.brands}</h3>
-        //                 <img src="${product.image_front_url}>
-        //                 <p><span>Ingredients<span>${product.ingredients}</p>
-        //                 <p><span>Quantity<span>${product.serving_size}</p>
-        //                 <p><span>Nutri-Score<span>${product.nutriscore_score}
-        //                 </p>`
-        console.log(result.data.product);
+        let product = result.data.product;
+        if(product){
+            foodResult.innerHTML = `<h2>${product.product_name}</h2>
+                        <h3>Brand : ${product.brands}</h3>
+                        <img src="${product.image_front_url}">
+                        <p>Ingredients : ${product.ingredients_text }</p>
+                        <p>Quantity : ${product.serving_size}</p>
+                        <p>Nutri : ${product.nutriscore_score}
+                        </p>`
+        foodResult.style.display="block";
+        localStorage.setItem("history",input.value);
+        let history = localStorage.getItem("history");
+        history.innerHTML = "history";
+        input.value = "";
+        }else{
+            alert("Product not found");
+        }
+
     }
     catch(error){
+        alert("Check your internet connection");
         console.log("Error fetching data",error);
     }
 }
